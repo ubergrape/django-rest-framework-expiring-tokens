03da5d48547399472658fffb71605bf5ccea294e
Riccardo Salzer <salzamt@live.com>
2019-02-05 18:36:14 +0100

    naming

--- rest_framework_expiring_authtoken/views.py
+++ rest_framework_expiring_authtoken/views.py
@@ -12,13 +12,13 @@ from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_204_NO_CONTENT
 from rest_framework_expiring_authtoken.models import ExpiringToken
 
 
-class ObtainExpiringAuthToken(ObtainAuthToken):
+class ExpiringAuthToken(ObtainAuthToken):
 
     """View enabling username/password exchange for expiring token."""
 
     model = ExpiringToken
 
-    def post(self, request):
+    def post(self, request, *args, **kwargs):
         """Respond to POSTed username/password with token."""
         serializer = AuthTokenSerializer(data=request.data)
 
@@ -55,4 +55,4 @@ class ObtainExpiringAuthToken(ObtainAuthToken):
         return Response(status=HTTP_204_NO_CONTENT)
 
 
-auth_token_view = ObtainExpiringAuthToken.as_view()
+auth_token_view = ExpiringAuthToken.as_view()
